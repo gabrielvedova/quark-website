4976d6596ceff633d7c70121a33a654fee952658
Caio de Araújo <caiotdearaujo@gmail.com>
2024-11-08 12:15:39 -0300

    fix(change-email): trim whitespace from email fields in validation schema

--- quark-website/app/api/admin/change-email/schema.ts
+++ quark-website/app/api/admin/change-email/schema.ts
@@ -1,9 +1,12 @@
 import { z } from "zod";
 
 export const PatchSchema = z.object({
-  email: z.string().email({ message: "Insira um email válido" }),
+  email: z.string().email({ message: "Insira um email válido" }).trim(),
 
-  newEmail: z.string().email({ message: "Insira um email válido" }),
+  newEmail: z.string().email({ message: "Insira um email válido" }).trim(),
 
-  newEmailConfirmation: z.string().email({ message: "Insira um email válido" }),
+  newEmailConfirmation: z
+    .string()
+    .email({ message: "Insira um email válido" })
+    .trim(),
 });
