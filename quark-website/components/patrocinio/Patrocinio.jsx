3a666bbd78b7b6d805165150debd2450c498592f
gabrielvedova <bielvedova@gmail.com>
2024-11-15 18:10:46 -0300

    fix(main-page): finished website structure

--- quark-website/components/patrocinio/Patrocinio.jsx
+++ quark-website/components/patrocinio/Patrocinio.jsx
@@ -2,5 +2,37 @@ import React from "react";
 import styles from "./Patrocinio.module.css";
 
 export default function Patrocinio() {
-  return <div className={styles.container}></div>;
+  const parceiros = [
+    {
+      id: 1,
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQAFRRS2ckze-ytk2D5Y6cs1WQJosqhcexSmA&s",
+    },
+    {
+      id: 2,
+      img: "https://www.amepe.com.br/site/docs/convenio/logos/0164382569353.png",
+    },
+    {
+      id: 3,
+      img: "https://portais.univasf.edu.br/cartes/imagens/ifpe.png/@@images/4d3ee7ba-c865-4bd5-87fd-0e51fa19fbda.png",
+    },
+    {
+      id: 4,
+      img: "https://pixeldots.com.br/wp-content/uploads/2023/05/Marca_Porto_Digital_2019.png",
+    },
+  ];
+  return (
+    <div className={styles.container}>
+      <div className={styles.title}>
+        <span>Parceiros</span>
+        <h1>Quem confia na Quark</h1>
+      </div>
+      <div className={styles.parceirosContainer}>
+        {parceiros.map((parceiro) => (
+          <div key={parceiro.id} className={styles.parceiro}>
+            <img src={parceiro.img} alt="Parceiro" />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
--- quark-website/components/time/Time.jsx
+++ quark-website/components/time/Time.jsx
@@ -1,6 +1,81 @@
 import React from "react";
 import styles from "./Time.module.css";
+import { FaLinkedin } from "react-icons/fa6";
 
 export default function Time() {
-  return <div className={styles.container} id="Time"></div>;
+  const time = [
+    {
+      id: 1,
+      name: "Ana Uriarte",
+      function: "CoFounder",
+      image:
+        "https://passport-photo.online/_optimized/prepare2.0498e1e2-opt-1920.WEBP",
+      linkedin: "https://www.linkedin.com/in/ana-uriarte-1b1b1b1b1/",
+    },
+    {
+      id: 2,
+      name: "João Tompson",
+      function: "CoFounder",
+      image:
+        "https://passport-photo.online/_optimized/prepare2.0498e1e2-opt-1920.WEBP",
+      linkedin: "https://www.linkedin.com/in/joão-tompson-2b2b2b2b2/",
+    },
+    {
+      id: 3,
+      name: "Felipe Ferraz",
+      function: "CTO",
+      image:
+        "https://passport-photo.online/_optimized/prepare2.0498e1e2-opt-1920.WEBP",
+      linkedin: "https://www.linkedin.com/in/felipe-ferraz-3b3b3b3b3/",
+    },
+    {
+      id: 4,
+      name: "Eudes Tenório",
+      function: "Desenvolvedor",
+      image:
+        "https://passport-photo.online/_optimized/prepare2.0498e1e2-opt-1920.WEBP",
+      linkedin: "https://www.linkedin.com/in/eudes-tenorio",
+    },
+    {
+      id: 5,
+      name: "Jarbas Agra",
+      function: "Designer",
+      image:
+        "https://passport-photo.online/_optimized/prepare2.0498e1e2-opt-1920.WEBP",
+      linkedin: "https://www.linkedin.com/in/jarbas-agra",
+    },
+    {
+      id: 6,
+      name: "Rennan Raffaele",
+      function: "Game Designer",
+      image:
+        "https://passport-photo.online/_optimized/prepare2.0498e1e2-opt-1920.WEBP",
+      linkedin: "https://www.linkedin.com/in/lucas-andrade",
+    },
+  ];
+
+  return (
+    <div className={styles.container} id="Time">
+      <div className={styles.title}>
+        <span>Time</span>
+        <h1>Quem faz a Quark</h1>
+      </div>
+      <div className={styles.timeContainer}>
+        {time.map((member) => {
+          return (
+            <div key={member.id} className={styles.member}>
+              <img src={member.image} alt={member.name} />
+              <div className={styles.memberDetails}>
+                <div className={styles.memberName}>
+                  <h3>{member.name}</h3>
+                  <p>{member.function}</p>
+                </div>
+                <FaLinkedin size={24} color="#0e76a8" href={member.linkedin} />
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
