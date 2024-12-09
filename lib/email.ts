434ea686c231994667d98f969cc00c2822bdc87f
Caio de Araújo <caiotdearaujo@gmail.com>
2024-12-09 14:16:38 -0300

    refactor(email): refactor sendContactEmail function to streamline email sending process with AWS SES

--- lib/email.ts
+++ lib/email.ts
@@ -8,33 +8,32 @@ export async function sendContactEmail(data: {
   institution: string;
 }) {
   const { name, email, phoneNumber, institution } = data;
+  const params = {
+    Destination: {
+      ToAddresses: [process.env.CONTACT_RECIPIENT_ADDRESS || ""],
+    },
+    Message: {
+      Subject: {
+        Data: `Solicitação de contato de ${name} da instituição ${institution}`,
+      },
+      Body: {
+        Html: {
+          Data: `
+        <ul>
+          <li><strong>Nome:</strong> ${name}</li>
+          <li><strong>Email:</strong> ${email}</li>
+          <li><strong>Telefone:</strong> ${phoneNumber}</li>
+          <li><strong>Instituição:</strong> ${institution}</li>
+        </ul>
+        `,
+        },
+      },
+    },
+    Source: email,
+  };
 
   try {
-    await ses
-      .sendEmail({
-        Destination: {
-          ToAddresses: [process.env.CONTACT_RECIPIENT_ADDRESS || ""],
-        },
-        Message: {
-          Subject: {
-            Data: `Solicitação de contato de ${name} da instituição ${institution}`,
-          },
-          Body: {
-            Html: {
-              Data: `
-            <ul>
-              <li><strong>Nome:</strong> ${name}</li>
-              <li><strong>Email:</strong> ${email}</li>
-              <li><strong>Telefone:</strong> ${phoneNumber}</li>
-              <li><strong>Instituição:</strong> ${institution}</li>
-            </ul>
-            `,
-            },
-          },
-        },
-        Source: email,
-      })
-      .promise();
+    await ses.sendEmail(params).promise();
   } catch (error) {
     throw new EmailSendingError();
   }
