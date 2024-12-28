495cf300ff2f20630d602939f153678143b181c5
gabrielvedova <bielvedova@gmail.com>
2024-12-28 18:15:45 -0300

    feat(Layout): add favicon to the application layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
 import "./styles.css";
+import favicon from "@/public/favicon-16x16.png";
 
 export const metadata = {
   title: "Quark",
@@ -7,6 +8,9 @@ export const metadata = {
 const RootLayout = ({ children }) => {
   return (
     <html lang="pt-br">
+      <head>
+        <link rel="icon" href={favicon.src} type="image/x-icon" />
+      </head>
       <body>
         <div id="root">{children}</div>
       </body>
