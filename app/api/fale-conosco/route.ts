2c67b493a7075fd85116022feecc3ff8a2173d29
Caio de Araújo <caiotdearaujo@gmail.com>
2024-12-30 17:28:31 -0300

    feat(contact): implement contact email sending functionality and improve API response handling

--- app/api/fale-conosco/route.ts
+++ app/api/fale-conosco/route.ts
@@ -1,34 +1,34 @@
-// import { ConventionalResponse } from "@/lib/responses";
-// import { PostSchema } from "./schema";
-// import { sendContactEmail } from "@/lib/email";
-// import { EmailSendingError } from "@/lib/errors";
+import { ConventionalResponse } from "@/lib/responses";
+import { PostSchema } from "./schema";
+import { sendContactEmail } from "@/lib/email";
+import { EmailSendingError } from "@/lib/errors";
 
-// export const OPTIONS = async () => {
-//   return ConventionalResponse.ok({ data: { methods: ["POST"] } });
-// };
+export const OPTIONS = async () => {
+  return ConventionalResponse.ok({ data: { methods: ["POST"] } });
+};
 
-// export const POST = async (request: Request) => {
-//   const body = await request.json();
-//   const validatedBody = PostSchema.safeParse(body);
+export const POST = async (request: Request) => {
+  const body = await request.json();
+  const validatedBody = PostSchema.safeParse(body);
 
-//   if (!validatedBody.success) {
-//     return ConventionalResponse.badRequest({
-//       error: validatedBody.error.flatten().fieldErrors,
-//     });
-//   }
+  if (!validatedBody.success) {
+    return ConventionalResponse.badRequest({
+      error: validatedBody.error.flatten().fieldErrors,
+    });
+  }
 
-//   try {
-//     await sendContactEmail(validatedBody.data);
-//     return ConventionalResponse.ok({
-//       message: "Informações enviadas com sucesso.",
-//     });
-//   } catch (error) {
-//     if (error instanceof EmailSendingError) {
-//       return ConventionalResponse.internalServerError({
-//         message: "Ocorreu um erro no envio do email.",
-//       });
-//     }
+  try {
+    await sendContactEmail(validatedBody.data);
+    return ConventionalResponse.ok({
+      message: "Informações enviadas com sucesso.",
+    });
+  } catch (error) {
+    if (error instanceof EmailSendingError) {
+      return ConventionalResponse.internalServerError({
+        message: "Ocorreu um erro no envio do email.",
+      });
+    }
 
-//     return ConventionalResponse.internalServerError();
-//   }
-// };
+    return ConventionalResponse.internalServerError();
+  }
+};
--- lib/email.ts
+++ lib/email.ts
@@ -1,42 +1,44 @@
-// import { EmailSendingError } from "./errors";
-// import sesClient from "./ses";
+import { SendEmailCommand } from "@aws-sdk/client-ses";
+import { EmailSendingError } from "./errors";
+import sesClient from "./ses";
 
-// export async function sendContactEmail(data: {
-//   name: string;
-//   email: string;
-//   phoneNumber: string;
-//   institution: string;
-//   reason: string;
-// }) {
-//   const { name, email, phoneNumber, institution, reason } = data;
-//   const params = {
-//     Destination: {
-//       ToAddresses: [process.env.CONTACT_RECIPIENT_ADDRESS || ""],
-//     },
-//     Message: {
-//       Subject: {
-//         Data: `Solicitação de contato de ${name} da instituição ${institution}`,
-//       },
-//       Body: {
-//         Html: {
-//           Data: `
-//         <ul>
-//           <li><strong>Nome:</strong> ${name}</li>
-//           <li><strong>Email:</strong> ${email}</li>
-//           <li><strong>Telefone:</strong> ${phoneNumber}</li>
-//           <li><strong>Instituição:</strong> ${institution}</li>
-//           <li><strong>Motivo:</strong> ${reason}</li>
-//         </ul>
-//         `,
-//         },
-//       },
-//     },
-//     Source: email,
-//   };
+export async function sendContactEmail(data: {
+  name: string;
+  email: string;
+  phoneNumber: string;
+  institution: string;
+  reason?: string;
+}) {
+  const { name, email, phoneNumber, institution, reason } = data;
+  const params = {
+    Destination: {
+      ToAddresses: [process.env.CONTACT_RECIPIENT_ADDRESS || ""],
+    },
+    Message: {
+      Subject: {
+        Data: `Solicitação de contato de ${name} da instituição ${institution}`,
+      },
+      Body: {
+        Html: {
+          Data: `
+        <ul>
+          <li><strong>Nome:</strong> ${name}</li>
+          <li><strong>Email:</strong> ${email}</li>
+          <li><strong>Telefone:</strong> ${phoneNumber}</li>
+          <li><strong>Instituição:</strong> ${institution}</li>
+          <li><strong>Motivo:</strong> ${reason}</li>
+        </ul>
+        `,
+        },
+      },
+    },
+    Source: email,
+  };
 
-//   try {
-//     await sesClient.sendEmail(params).promise();
-//   } catch (error) {
-//     throw new EmailSendingError();
-//   }
-// }
+  try {
+    const command = new SendEmailCommand(params);
+    await sesClient.send(command);
+  } catch (error) {
+    throw new EmailSendingError();
+  }
+}
