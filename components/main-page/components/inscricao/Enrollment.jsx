f73cb47a616c4dcac0933a970ea1365790ab3e09
gabrielvedova <bielvedova@gmail.com>
2024-12-31 15:35:15 -0300

    feat(Enrollment): enhance form validation and error handling for user inputs

--- components/main-page/components/inscricao/Enrollment.jsx
+++ components/main-page/components/inscricao/Enrollment.jsx
@@ -1,8 +1,7 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import "./Enrollment.css";
 import { IoIosClose } from "react-icons/io";
-import { set } from "date-fns";
 
 export default function Enrollment(props) {
   const [name, setName] = useState("");
@@ -10,17 +9,21 @@ export default function Enrollment(props) {
   const [email, setEmail] = useState("");
   const [institution, setInstitution] = useState("");
   const [reason, setReason] = useState("");
+  const [errors, setErrors] = useState({
+    name: "",
+    institution: "",
+  });
+
+  const nameRef = useRef(null);
+  const phoneRef = useRef(null);
+  const emailRef = useRef(null);
+  const institutionRef = useRef(null);
+  const reasonRef = useRef(null);
 
   async function sendForm() {
-    const name = name;
-    const phone = phone;
-    const email = email;
-    const institution = institution;
-    const reason = reason;
-
     const data = {
       name,
-      phone,
+      phoneNumber: phone,
       email,
       institution,
       reason,
@@ -46,6 +49,18 @@ export default function Enrollment(props) {
       }
     } catch (error) {
       console.error("Erro na requisição:", error);
+    } finally {
+      props.setter(false);
+      setName("");
+      setPhone("");
+      setEmail("");
+      setInstitution("");
+      setReason("");
+      if (nameRef.current) nameRef.current.value = "";
+      if (phoneRef.current) phoneRef.current.value = "";
+      if (emailRef.current) emailRef.current.value = "";
+      if (institutionRef.current) institutionRef.current.value = "";
+      if (reasonRef.current) reasonRef.current.value = "";
     }
   }
 
@@ -59,28 +74,54 @@ export default function Enrollment(props) {
         <button id="closeContainer" onClick={() => props.setter(!props.forms)}>
           <IoIosClose size={70} color="#d5d5d5" />
         </button>
-        <form className="mainEnrollment" onSubmit={sendForm}>
+        <div className="mainEnrollment">
           <div className="itemEnrollment">
             <div>Nome</div>
             <input
               type="text"
               className="input"
               id="name"
-              onBlur={() => {
-                if (document.querySelector("#name").value.length < 3) {
-                  document.querySelector("#name").style.border =
-                    "1px solid red";
-                  document.querySelector("#name + span").style.display =
-                    "block";
+              ref={nameRef}
+              onBlur={(e) => {
+                const regex = /^[A-Za-zÀ-ÿ\u0100-\u017F\s]+$/;
+                const value = e.target.value;
+
+                if (value.length < 3) {
+                  e.target.style.border = "1px solid red";
+                  e.target.nextElementSibling.style.display = "block";
                   setName("");
+                  setErrors({
+                    ...errors,
+                    name: "Nome deve ter pelo menos 3 caracteres.",
+                  });
+                } else if (value.length > 100) {
+                  e.target.style.border = "1px solid red";
+                  e.target.nextElementSibling.style.display = "block";
+                  setName("");
+                  setErrors({
+                    ...errors,
+                    name: "Nome deve ter no máximo 100 caracteres.",
+                  });
+                } else if (!regex.test(value)) {
+                  e.target.style.border = "1px solid red";
+                  e.target.nextElementSibling.style.display = "block";
+                  setName("");
+                  setErrors({
+                    ...errors,
+                    name: "Nome deve possuir apenas letras e espaços.",
+                  });
                 } else {
-                  document.querySelector("#name").style.border = "none";
-                  document.querySelector("#name + span").style.display = "none";
-                  setName(document.querySelector("#name").value);
+                  e.target.style.border = "none";
+                  e.target.nextElementSibling.style.display = "none";
+                  setName(value);
+                  setErrors({
+                    ...errors,
+                    name: "",
+                  });
                 }
               }}
             />
-            <span>Nome Inválido</span>
+            <span>{errors.name}</span>
           </div>
           <div className="itemEnrollment">
             <div>Telefone</div>
@@ -88,12 +129,13 @@ export default function Enrollment(props) {
               type="tel"
               className="input"
               id="phone"
+              ref={phoneRef}
               maxLength={11}
               onBlur={(e) => {
                 let phone = e.target.value.replace(/\D/g, "");
 
                 e.target.value =
-                  phone.length === 11
+                  phone.length === 11 && phone[2] === "9"
                     ? (phone = `(${phone.slice(0, 2)}) ${phone.slice(
                         2,
                         7
@@ -116,7 +158,7 @@ export default function Enrollment(props) {
                 }
               }}
             />
-            <span>Telefone Inválido</span>
+            <span>Número de telefone inválido</span>
           </div>
           <div className="itemEnrollment">
             <div>E-mail</div>
@@ -124,6 +166,7 @@ export default function Enrollment(props) {
               type="email"
               className="input"
               id="e-mail"
+              ref={emailRef}
               onBlur={() => {
                 const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
                 const validEmail = regex.test(
@@ -152,6 +195,7 @@ export default function Enrollment(props) {
               type="text"
               className="input"
               id="institution"
+              ref={institutionRef}
               onBlur={() => {
                 if (document.querySelector("#institution").value.length < 3) {
                   document.querySelector("#institution").style.border =
@@ -159,6 +203,24 @@ export default function Enrollment(props) {
                   document.querySelector("#institution + span").style.display =
                     "block";
                   setInstitution("");
+                  setErrors({
+                    ...errors,
+                    institution:
+                      "Instituição deve ter pelo menos 3 caracteres.",
+                  });
+                } else if (
+                  document.querySelector("#institution").value.length > 200
+                ) {
+                  document.querySelector("#institution").style.border =
+                    "1px solid red";
+                  document.querySelector("#institution + span").style.display =
+                    "block";
+                  setInstitution("");
+                  setErrors({
+                    ...errors,
+                    institution:
+                      "Instituição deve ter no máximo 200 caracteres.",
+                  });
                 } else {
                   document.querySelector("#institution").style.border = "none";
                   document.querySelector("#institution + span").style.display =
@@ -167,7 +229,7 @@ export default function Enrollment(props) {
                 }
               }}
             />
-            <span>Instituição Inválida</span>
+            <span>{errors.institution}</span>
           </div>
           <div className="itemEnrollment">
             <div>Motivo de Incrição</div>
@@ -175,17 +237,18 @@ export default function Enrollment(props) {
               name=""
               rows={3}
               className="input"
+              ref={reasonRef}
               onChange={(e) => setReason(e.target.value)}
             ></textarea>
           </div>
           <button
             id="formsSend"
-            type="submit"
             disabled={!(name && phone && email && institution)}
+            onClick={sendForm}
           >
             Enviar
           </button>
-        </form>
+        </div>
       </div>
     </div>
   );
